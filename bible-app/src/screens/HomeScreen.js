7301df8fe5e9c72123bfce7531b4b1a7519a7fae
unknown <faleyecaleb@gmail.com>
2022-05-25 12:49:20 -0700

    second build

--- bible-app/src/screens/HomeScreen.js
+++ bible-app/src/screens/HomeScreen.js
@@ -1,15 +1,17 @@
-import { Text, View, SafeAreaView, FlatList } from 'react-native'
+import { Text, View, SafeAreaView, StatusBar, FlatList } from 'react-native'
 import WeeksCard from '../components/WeeksCard'
+import COLORS from '../theme/colors'
 
-const dataList = ['Week One', 'Week Two', 'Week Three', 'Week Four', 'Week Five', 'Week Six', 'Week Seven', 'Week Eight', 'Week Nine', 'Week Ten']
+const dataList = ['Week One', 'Week Two', 'Week Three', 'Week Four', 'Week Five', 'Week Six', 'Week Seven', 'Week Eight', 'Week Nine', 'Week Ten', 'djkslf', 'ksljdfs', 'dksd']
 
 const HomeScreen = () => {
   return(
     <SafeAreaView>
-      <View>
-        
+      <StatusBar translucent={false} animated={true} backgroundColor={COLORS.gradientLight} barStyle='dark-content' />
+      <View style={{flex: 1, paddingVertical: 10,}}>
+        <Text>Table Of Contents</Text>
       </View>
-      <FlatList data={dataList} renderItem={(data) => <WeeksCard />} />
+      <FlatList data={dataList} stickyHeaderIndices={[0]} renderItem={(data) => <WeeksCard />} />
     </SafeAreaView>
   )
 }
