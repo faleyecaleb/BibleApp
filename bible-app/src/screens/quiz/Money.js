b6f13406f1c78a3445348f1a0947a013840d0684
unknown <faleyecaleb@gmail.com>
2022-06-07 13:28:30 -0700

    added money data

--- bible-app/src/screens/quiz/Money.js
+++ bible-app/src/screens/quiz/Money.js
@@ -2,6 +2,23 @@ import { View, Text } from 'react-native'
 import React from 'react'
 
 const Money = () => {
+  const money = [
+    {id: 1, amount: '$ 100'},
+    {id: 2, amount: '$ 200'},
+    {id: 3, amount: '$ 300'},
+    {id: 4, amount: '$ 500'},
+    {id: 5, amount: '$ 1000'},
+    {id: 6, amount: '$ 2000'},
+    {id: 7, amount: '$ 4000'},
+    {id: 8, amount: '$ 8000'},
+    {id: 1, amount: '$ 16000'},
+    {id: 1, amount: '$ 32000'},
+    {id: 1, amount: '$ 64000'},
+    {id: 1, amount: '$ 125000'},
+    {id: 1, amount: '$ 250000'},
+    {id: 1, amount: '$ 500000'},
+    {id: 1, amount: '$ 1000000'},
+  ]
   return (
     <View>
       <Text>Money</Text>
--- bible-app/src/screens/quiz/Quiz.js
+++ bible-app/src/screens/quiz/Quiz.js
@@ -1,13 +1,27 @@
-import { View, Text, SafeAreaView, ImageBackground, } from 'react-native'
+import { View, Text, SafeAreaView, ImageBackground, StyleSheet, Dimensions, StatusBar } from 'react-native'
 import React from 'react'
 import Money from './Money'
+import COLORS from '../../consts/colors';
+
+
+const height = Dimensions.get('screen').height;
 
 const Quiz = () => {
+  const img = require('../../assets/images/quizImg.jpg')
   return (
     <SafeAreaView>
-      <Money />
+      <StatusBar animated={true} backgroundColor={COLORS.dark} barStyle={'light-content'} />
+      <ImageBackground resizeMode='cover' style={styles.backgrounImage} source={img}>
+        <Text style={{color: COLORS.white}}>THis is awesome</Text>
+      </ImageBackground>
     </SafeAreaView>
   )
 }
+
+const styles = StyleSheet.create({
+  backgrounImage: {
+    height: height
+  }
+})
 
 export default Quiz
