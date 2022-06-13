42d32744284f00356b73d8655e2ae24505127f51
unknown <faleyecaleb@gmail.com>
2022-06-13 12:52:49 -0700

    i just finished the first screen ui design

--- bible-app/src/screens/FirstScreen.js
+++ bible-app/src/screens/FirstScreen.js
@@ -18,9 +18,64 @@ const imageW = width * 0.7;
 const imageH = imageW * 1.54;
 
 const FirstScreen = () => {
+  const scrollX = React.useRef(new Animated.Value(0)).current;
   return (
     <View style={{ flex: 1, backgroundColor: '#000' }}>
-      <StatusBar hidden />
+      <StatusBar animated={true} />
+      <View style={StyleSheet.absoluteFillObject}>
+        {
+          data.map((image, index) => {
+            const inputRange = [
+              (index - 1) * width,
+              index * width,
+              (index + 1) * width,
+            ]
+            const opacity = scrollX.interpolate({
+              inputRange,
+              outputRange: [0, 1, 0]
+            })
+            return <Animated.Image 
+                  key={`image-${index}`}
+                  source={{uri: image}}
+                  style={[
+                    StyleSheet.absoluteFillObject,
+                    {
+                      opacity
+                    }
+                  ]}
+                  blurRadius={50}
+            />
+          })
+        }
+      </View>
+      <Animated.FlatList
+      horizontal
+      pagingEnabled
+      onScroll={Animated.event(
+        [{nativeEvent: {contentOffset: {x: scrollX}}}],
+        {useNativeDriver: true}
+      )}
+        data={data}
+        keyExtractor={(_, index) => index.toString()}
+        renderItem={({item}) => {
+          return <View style={{width, justifyContent: 'center', alignItems: 'center',
+                shadowColor: '#000',
+                shadowOpacity: 1,
+                shadowOffset: {
+                  width: 0,
+                  height: 0,
+                },
+                shadowRadius: 20,
+          }}>
+            <Image source={{uri: item}} style={{
+              width: imageW,
+              height: imageH,
+              resizeMode: 'cover',
+              borderRadius: 16,
+            }}/>
+          </View>
+        }}
+      />
     </View>
   )
 }
